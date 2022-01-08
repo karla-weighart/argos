3192d1af7398739e1265ca4c6a10009f0fcce8c7
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2022-01-08 14:31:44 +0100

    Fix yolov3 paths

--- lib/yolov3/src/yolov3/models/common.py
+++ lib/yolov3/src/yolov3/models/common.py
@@ -19,11 +19,11 @@ import torch.nn as nn
 from PIL import Image
 from torch.cuda import amp
 
-from utils.datasets import exif_transpose, letterbox
-from utils.general import (LOGGER, check_requirements, check_suffix, colorstr, increment_path, make_divisible,
+from yolov3.utils.datasets import exif_transpose, letterbox
+from yolov3.utils.general import (LOGGER, check_requirements, check_suffix, colorstr, increment_path, make_divisible,
                            non_max_suppression, scale_coords, xywh2xyxy, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import time_sync
+from yolov3.utils.plots import Annotator, colors, save_one_box
+from yolov3.utils.torch_utils import time_sync
 
 
 def autopad(k, p=None):  # kernel, padding
--- lib/yolov3/src/yolov3/models/experimental.py
+++ lib/yolov3/src/yolov3/models/experimental.py
@@ -8,8 +8,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from models.common import Conv
-from utils.downloads import attempt_download
+from yolov3.models.common import Conv
+from yolov3.utils.downloads import attempt_download
 
 
 class CrossConv(nn.Module):
